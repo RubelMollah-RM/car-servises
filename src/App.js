97213da3f58aef8908cd79d5d545f01abbcec673
RubelMollah-RM <rubelmollah9311@gmail.com>
2022-10-18 07:59:46 +0600

    data loaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home/Home';
+import Header from './pages/Shared/Header/Header';
+import Footer from './pages/Shared/Footer/Footer';
 
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/pages/Home/Servises/Servises.js
+++ src/pages/Home/Servises/Servises.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Servises = () => {
+    const [servise, setServise] = useState([]);
+
+    useEffect( () =>{
+        fetch("servises.json")
+        .then(res => res.json())
+        .then(data => setServise(data))
+    },[])
     return (
         <div>
-            <h2>servises</h2>
+            <h2>servises : {servise.length}</h2>
         </div>
     );
 };
